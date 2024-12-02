015df1f035a8e077327a8efcaeb29c0c81bbf613
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-02 09:01:31 -0500

    d2p2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,12 +9,17 @@ fn main() -> anyhow::Result<()> {
         })
         .collect();
 
-    let safe_count = input
+    let safe_count_1 = input
         .iter()
         .filter(|report| safe_part_1(report.as_slice()))
         .count();
+    println!("Part 1 safe count: {}", safe_count_1);
 
-    println!("Part 1 safe count: {}", safe_count);
+    let safe_count_2 = input
+        .iter()
+        .filter(|report| safe_part_2(report.as_slice()))
+        .count();
+    println!("Part 2 safe count: {}", safe_count_2);
 
     Ok(())
 }
@@ -23,7 +28,27 @@ fn safe_part_1(report: &[i32]) -> bool {
     let diffs = || report.windows(2).map(|v| v[1] - v[0]);
 
     let monotonic = diffs().all(|d| d < 0) || diffs().all(|d| d > 0);
-    let magnitudes = diffs().all(|d| d.abs() >= 1 && d.abs() <= 3);
+    let magnitudes = diffs().all(|d| (1..=3).contains(&d.abs()));
 
     monotonic && magnitudes
+}
+
+fn safe_part_2(report: &[i32]) -> bool {
+    // check original case
+    if safe_part_1(report) {
+        return true;
+    }
+
+    // brute force removing one element at a time, and not worrying about
+    // efficiency (like allocations) at all...
+    for idx_removed in 0..report.len() {
+        let mut report = report.to_vec();
+        report.remove(idx_removed);
+
+        if safe_part_1(&report) {
+            return true;
+        }
+    }
+
+    false
 }

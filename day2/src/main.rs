11f622f74853a2f696dfc933ec07efc70e4f0c08
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-02 08:46:45 -0500

    d2p1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let text = common::read_file("input1.txt")?;
+    let input: Vec<_> = text
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    let safe_count = input
+        .iter()
+        .filter(|report| safe_part_1(report.as_slice()))
+        .count();
+
+    println!("Part 1 safe count: {}", safe_count);
+
+    Ok(())
+}
+
+fn safe_part_1(report: &[i32]) -> bool {
+    let diffs = || report.windows(2).map(|v| v[1] - v[0]);
+
+    let monotonic = diffs().all(|d| d < 0) || diffs().all(|d| d > 0);
+    let magnitudes = diffs().all(|d| d.abs() >= 1 && d.abs() <= 3);
+
+    monotonic && magnitudes
 }

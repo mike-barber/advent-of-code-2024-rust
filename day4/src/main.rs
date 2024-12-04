ad856dea1eaf9f5d004b0bf7e802808f6db407ad
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-04 08:06:01 -0500

    cleanup

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -9,13 +9,18 @@ struct Problem {
 }
 
 impl Problem {
-    fn characters(&self, x: usize, y: usize, dir: Dir, buf: &mut String) {
+    fn characters(&self, x: usize, y: usize, max_length: usize, dir: Dir, buf: &mut String) {
         buf.clear();
 
         let mut x = x as i32;
         let mut y = y as i32;
 
-        while x >= 0 && x < self.cols as i32 && y >= 0 && y < self.rows as i32 {
+        while x >= 0
+            && x < self.cols as i32
+            && y >= 0
+            && y < self.rows as i32
+            && buf.len() < max_length
+        {
             buf.push(self.matrix[y as usize][x as usize]);
             x += dir.0;
             y += dir.1;
@@ -37,19 +42,6 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn directions() -> Vec<Dir> {
-    let mut dirs = vec![];
-    for x in -1..=1 {
-        for y in -1..=1 {
-            if x == 0 && y == 0 {
-                continue;
-            }
-            dirs.push(Dir(x, y));
-        }
-    }
-    dirs
-}
-
 fn parse(input: &str) -> Problem {
     let matrix: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let rows = matrix.len();
@@ -59,15 +51,15 @@ fn parse(input: &str) -> Problem {
 
 // not optimal by any means, but it's small enough to work
 fn part1(problem: &Problem) -> usize {
-    let dirs = directions();
+    let dirs = [Dir(0, 1), Dir(1, 0), Dir(1, 1), Dir(1, -1)];
 
     let mut buf = String::new();
     let mut count = 0;
     for dir in &dirs {
         for x in 0..problem.cols {
             for y in 0..problem.rows {
-                problem.characters(x, y, *dir, &mut buf);
-                let found = buf.starts_with("XMAS");
+                problem.characters(x, y, 4, *dir, &mut buf);
+                let found = buf == "XMAS" || buf == "SAMX";
                 if found {
                     count += 1;
                 }

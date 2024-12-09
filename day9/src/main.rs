34a32e8eb46fc2f4aa9ccfc0a4f73690fa61008a
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-08 22:59:31 -0500

    prep

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Instant;
+
+use anyhow::Result;
+
+#[derive(Debug, Clone)]
+pub struct Problem {}
+
+fn parse_input(input: &str) -> Result<Problem> {
+    Ok(Problem {})
+}
+
+fn part1(problem: &Problem) -> Result<usize> {
+    Ok(1)
+}
+
+fn part2(problem: &Problem) -> Result<usize> {
+    Ok(2)
+}
+
+fn main() -> anyhow::Result<()> {
+    let text = common::read_file("input1.txt")?;
+    let problem = parse_input(&text)?;
+
+    let t1 = Instant::now();
+    let count_part1 = part1(&problem)?;
+    println!("Part 1 result is {count_part1} (took {:?})", t1.elapsed());
+
+    let t2 = Instant::now();
+    let count_part2 = part2(&problem)?;
+    println!("Part 2 result is {count_part2} (took {:?})", t2.elapsed());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    const EXAMPLE: &str = indoc! {"
+    "};
+
+    #[test]
+    fn test_parse_input() -> Result<()> {
+        let problem = parse_input(EXAMPLE)?;
+        println!("{:?}", problem);
+        Ok(())
+    }
+
+    #[test]
+    fn part1_correct() -> Result<()> {
+        let problem = parse_input(EXAMPLE)?;
+        let count = part1(&problem)?;
+        assert_eq!(count, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn part2_correct() -> Result<()> {
+        let problem = parse_input(EXAMPLE)?;
+        let count = part2(&problem)?;
+        assert_eq!(count, 2);
+        Ok(())
+    }
 }

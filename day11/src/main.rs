72a8f2f3dbcd18ab3d21593aae917896e2e9e383
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-11 01:28:27 -0500

    day 11 complete

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,20 +1,145 @@
-use std::time::Instant;
+use std::{
+    collections::HashMap,
+    iter,
+    ops::{Div, Rem},
+    time::Instant,
+};
 
 use anyhow::Result;
+use common::OptionAnyhow;
+use dlv_list::VecList;
 
 #[derive(Debug, Clone)]
-pub struct Problem {}
+pub struct Problem {
+    stones: VecList<i64>,
+}
 
 fn parse_input(input: &str) -> Result<Problem> {
-    Ok(Problem {})
+    let mut stones = VecList::new();
+    for n in input.split_whitespace() {
+        stones.push_back(n.parse()?);
+    }
+    Ok(Problem { stones })
+}
+
+fn try_split(n: i64) -> Option<(i64, i64)> {
+    let order = n.ilog10() + 1;
+    if order % 2 == 0 {
+        let factor = iter::successors(Some(1), |a| Some(a * 10))
+            .nth(order as usize / 2)
+            .expect("factor");
+        Some((n.div(factor), n.rem(factor)))
+    } else {
+        None
+    }
+}
+
+fn iterate(stones: &VecList<i64>, iterations: usize) -> Result<usize> {
+    let mut stones = stones.clone();
+    for _ in 0..iterations {
+        let mut ix = stones.front_index().ok_anyhow()?;
+        loop {
+            match stones.get(ix).copied().ok_anyhow()? {
+                0 => *stones.get_mut(ix).ok_anyhow()? = 1,
+                n => {
+                    if let Some((a, b)) = try_split(n) {
+                        stones.insert_before(ix, a);
+                        *stones.get_mut(ix).ok_anyhow()? = b;
+                    } else {
+                        *stones.get_mut(ix).ok_anyhow()? = n.checked_mul(2024).ok_anyhow()?;
+                    }
+                }
+            }
+            if let Some(next) = stones.get_next_index(ix) {
+                ix = next;
+            } else {
+                break;
+            }
+        }
+    }
+    let num_stones = stones.len();
+    Ok(num_stones)
 }
 
 fn part1(problem: &Problem) -> Result<usize> {
-    Ok(1)
+    iterate(&problem.stones, 25)
 }
 
-fn part2(problem: &Problem) -> Result<usize> {
-    Ok(2)
+fn iterate_recurse_count(n: i64, remaining_depth: usize) -> usize {
+    if remaining_depth == 0 {
+        return 1;
+    }
+    match n {
+        0 => {
+            let a = 1;
+            iterate_recurse_count(a, remaining_depth - 1)
+        }
+        n => {
+            if let Some((a, b)) = try_split(n) {
+                let num_a = iterate_recurse_count(a, remaining_depth - 1);
+                let num_b = iterate_recurse_count(b, remaining_depth - 1);
+                num_a + num_b
+            } else {
+                let a = n.checked_mul(2024).expect("overflow");
+                iterate_recurse_count(a, remaining_depth - 1)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct Key(i64, usize);
+
+/// Recursive with memoization
+fn iterate_recurse_count_mem(
+    n: i64,
+    remaining_depth: usize,
+    memory: &mut HashMap<Key, usize>,
+) -> usize {
+    // termination
+    if remaining_depth == 0 {
+        return 1;
+    }
+    
+    // already-computed value
+    if let Some(mem) = memory.get(&Key(n, remaining_depth)) {
+        return *mem;
+    }
+
+    // otherwise iterate
+    let count = match n {
+        0 => {
+            let a = 1;
+            iterate_recurse_count_mem(a, remaining_depth - 1, memory)
+        }
+        n => {
+            if let Some((a, b)) = try_split(n) {
+                let num_a = iterate_recurse_count_mem(a, remaining_depth - 1, memory);
+                let num_b = iterate_recurse_count_mem(b, remaining_depth - 1, memory);
+                num_a + num_b
+            } else {
+                let a = n.checked_mul(2024).expect("overflow");
+                iterate_recurse_count_mem(a, remaining_depth - 1, memory)
+            }
+        }
+    };
+
+    // store small n in the cache - all single digits
+    if n < 10 {
+        memory.insert(Key(n, remaining_depth), count);
+    }
+
+    count
+}
+
+fn part2(problem: &Problem, iterations: usize) -> Result<usize> {
+    // memory can be used across multiple calls
+    let mut mem = HashMap::new();
+    let mut total = 0;
+    for n in &problem.stones {
+        total += iterate_recurse_count_mem(*n, iterations, &mut mem);
+    }
+    Ok(total)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -25,8 +150,21 @@ fn main() -> anyhow::Result<()> {
     let count_part1 = part1(&problem)?;
     println!("Part 1 result is {count_part1} (took {:?})", t1.elapsed());
 
+  
+    // try iterate simple
+    let t = Instant::now();
+    let nn = iterate_recurse_count(0, 30);
+    println!("{nn} in {:?}", t.elapsed());
+
+    // try iterate memoized
+    let t = Instant::now();
+    let mut mem = HashMap::new();
+    let nn = iterate_recurse_count_mem(0, 30, &mut mem);
+    println!("{nn} in {:?}", t.elapsed());
+
+    // part 2 result
     let t2 = Instant::now();
-    let count_part2 = part2(&problem)?;
+    let count_part2 = part2(&problem, 75)?;
     println!("Part 2 result is {count_part2} (took {:?})", t2.elapsed());
 
     Ok(())
@@ -38,7 +176,15 @@ mod tests {
     use indoc::indoc;
 
     const EXAMPLE: &str = indoc! {"
+        125 17
     "};
+
+    #[test]
+    fn split_correct() {
+        assert_eq!(try_split(1000).unwrap(), (10, 0));
+        assert_eq!(try_split(10).unwrap(), (1, 0));
+        assert_eq!(try_split(111222).unwrap(), (111, 222));
+    }
 
     #[test]
     fn test_parse_input() -> Result<()> {
@@ -51,15 +197,15 @@ mod tests {
     fn part1_correct() -> Result<()> {
         let problem = parse_input(EXAMPLE)?;
         let count = part1(&problem)?;
-        assert_eq!(count, 1);
+        assert_eq!(count, 55312);
         Ok(())
     }
 
     #[test]
     fn part2_correct() -> Result<()> {
         let problem = parse_input(EXAMPLE)?;
-        let count = part2(&problem)?;
-        assert_eq!(count, 2);
+        let count = part2(&problem, 25)?;
+        assert_eq!(count, 55312);
         Ok(())
     }
 }

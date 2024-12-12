88f37c9c6cdecac594c9027b1824a23cd3c577b0
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-12 00:53:56 -0500

    d12p1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,16 +1,129 @@
-use std::time::Instant;
+use std::{convert::identity, time::Instant};
 
 use anyhow::Result;
+use common::cartesian::{matrix_from_lines, Point, ScreenDir};
+use nalgebra::DMatrix;
+
+type PlantMap = DMatrix<char>;
+type RegionMap = DMatrix<i32>;
 
 #[derive(Debug, Clone)]
-pub struct Problem {}
+pub struct Measurement {
+    area: usize,
+    perimeter: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct Problem {
+    plants: PlantMap,
+}
+impl Problem {
+    fn perimeter(&self, loc: Point) -> usize {
+        let mut perim = 0;
+        let ch = self.plants.get(loc).unwrap();
+        for n in neighbours(loc) {
+            perim += match self.plants.get(n) {
+                None => 1,
+                Some(nch) if nch == ch => 0,
+                Some(_) => 1,
+            }
+        }
+        perim
+    }
+
+    fn explore_region(&self, loc: Point, regions: &mut RegionMap, label: i32) -> Measurement {
+        let mut area = 0;
+        let mut perimeter = 0;
+
+        let mut queue = Vec::new();
+        queue.push(loc);
+
+        let plant = self.plants.get(loc).unwrap();
+        loop {
+            // explore next location
+            let current = match queue.pop() {
+                None => break,
+                Some(n) => n,
+            };
+
+            // only if not visited
+            if *regions.get(current).unwrap() != -1 {
+                continue;
+            }
+
+            // add area & record visited
+            area += 1;
+            perimeter += self.perimeter(current);
+            *regions.get_mut(current).unwrap() = label;
+            //println!("{current:?} {area}");
+
+            // find possible neighbours
+            for next in neighbours(current) {
+                // only unexplored
+                if let Some(r) = regions.get(next) {
+                    if *r != -1 {
+                        continue;
+                    }
+                }
+                // and only if it matches our plant type
+                if let Some(ch) = self.plants.get(next) {
+                    if ch == plant {
+                        queue.push(next);
+                    }
+                }
+            }
+        }
+
+        Measurement { area, perimeter }
+    }
+}
+
+fn offsets() -> [Point; 4] {
+    [
+        ScreenDir::U.into(),
+        ScreenDir::R.into(),
+        ScreenDir::D.into(),
+        ScreenDir::L.into(),
+    ]
+}
+
+fn neighbours(loc: Point) -> [Point; 4] {
+    [
+        loc + ScreenDir::U.into(),
+        loc + ScreenDir::R.into(),
+        loc + ScreenDir::D.into(),
+        loc + ScreenDir::L.into(),
+    ]
+}
 
 fn parse_input(input: &str) -> Result<Problem> {
-    Ok(Problem {})
+    let lines: Vec<_> = input.lines().collect();
+    let plants = matrix_from_lines(&lines, |a| Ok(a))?;
+    Ok(Problem { plants })
 }
 
 fn part1(problem: &Problem) -> Result<usize> {
-    Ok(1)
+    let mut total_price = 0;
+    let mut region_map =
+        RegionMap::from_element(problem.plants.nrows(), problem.plants.ncols(), -1);
+    
+    let mut label = 0;
+    for x in 0..problem.plants.ncols() {
+        for y in 0..problem.plants.nrows() {
+            let loc = Point::new(x as i64,y as i64);
+            if *region_map.get(loc).unwrap() == -1 {
+                // unexplored -- map this region
+                let measurement = problem.explore_region(loc, &mut region_map, label);
+                // println!("{loc:?} {measurement:?}");
+                // println!("{region_map}");
+                label += 1;
+                total_price += measurement.area * measurement.perimeter
+            }
+
+        }
+    }
+
+    Ok(total_price)
 }
 
 fn part2(problem: &Problem) -> Result<usize> {
@@ -38,6 +151,16 @@ mod tests {
     use indoc::indoc;
 
     const EXAMPLE: &str = indoc! {"
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
     "};
 
     #[test]
@@ -51,7 +174,7 @@ mod tests {
     fn part1_correct() -> Result<()> {
         let problem = parse_input(EXAMPLE)?;
         let count = part1(&problem)?;
-        assert_eq!(count, 1);
+        assert_eq!(count, 1930);
         Ok(())
     }
 

03bc07decff276662068d70719f15d693d00371f
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2024-12-17 14:06:24 -0500

    mini version of computer

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{path::Component, time::Instant};
 
 use anyhow::{bail, Result};
 use common::OptionAnyhow;
@@ -20,7 +20,7 @@ pub struct Computer {
     reg_c: i64,
     program: Vec<u8>,
     ip: usize,
-    output: Vec<u8>
+    output: Vec<u8>,
 }
 impl Computer {
     fn new(a: i64, b: i64, c: i64, program: Vec<u8>) -> Computer {
@@ -29,8 +29,8 @@ impl Computer {
             reg_b: b,
             reg_c: c,
             program,
-            ip: 0, 
-            output: vec![]
+            ip: 0,
+            output: vec![],
         }
     }
 }
@@ -50,7 +50,7 @@ fn parse_input(input: &str) -> Result<Computer> {
 
     // skip blank line
     it.next();
-    
+
     // read program
     let program_str = right_str(it.next().ok_anyhow()?)?;
     let mut program = vec![];
@@ -65,30 +65,29 @@ fn parse_input(input: &str) -> Result<Computer> {
         reg_c,
         program,
         ip: 0,
-        output: vec![]
+        output: vec![],
     })
 }
 
 impl Computer {
-
     /// Combo operands 0 through 3 represent literal values 0 through 3.
     /// Combo operand 4 represents the value of register A.
     /// Combo operand 5 represents the value of register B.
     /// Combo operand 6 represents the value of register C.
     /// Combo operand 7 is reserved and will not appear in valid programs.
-    fn combo_operand(&self, operand:i64) -> Option<i64> {
+    fn combo_operand(&self, operand: i64) -> Option<i64> {
         match operand {
             0..=3 => Some(operand),
             4 => Some(self.reg_a),
             5 => Some(self.reg_b),
             6 => Some(self.reg_c),
-            _ => None
+            _ => None,
         }
     }
 
-    /// The adv instruction (opcode 0) performs division. The numerator is the value in the A register. 
-    /// The denominator is found by raising 2 to the power of the instruction's combo operand. 
-    /// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) 
+    /// The adv instruction (opcode 0) performs division. The numerator is the value in the A register.
+    /// The denominator is found by raising 2 to the power of the instruction's combo operand.
+    /// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
     /// The result of the division operation is truncated to an integer and then written to the A register.
     fn adv(&mut self, operand: i64) {
         let operand = self.combo_operand(operand).unwrap();
@@ -96,7 +95,7 @@ impl Computer {
         self.ip += 2;
     }
 
-    /// The bdv instruction (opcode 6) works exactly like the adv instruction except that the 
+    /// The bdv instruction (opcode 6) works exactly like the adv instruction except that the
     /// result is stored in the B register. (The numerator is still read from the A register.)
     fn bdv(&mut self, operand: i64) {
         let operand = self.combo_operand(operand).unwrap();
@@ -104,7 +103,7 @@ impl Computer {
         self.ip += 2;
     }
 
-    /// The cdv instruction (opcode 7) works exactly like the adv instruction except that the 
+    /// The cdv instruction (opcode 7) works exactly like the adv instruction except that the
     /// result is stored in the C register. (The numerator is still read from the A register.)
     fn cdv(&mut self, operand: i64) {
         let operand = self.combo_operand(operand).unwrap();
@@ -112,14 +111,14 @@ impl Computer {
         self.ip += 2;
     }
 
-    /// The bxl instruction (opcode 1) calculates the bitwise XOR of register B 
+    /// The bxl instruction (opcode 1) calculates the bitwise XOR of register B
     /// and the instruction's literal operand, then stores the result in register B.
     fn bxl(&mut self, operand: i64) {
         self.reg_b ^= operand;
         self.ip += 2;
     }
 
-    /// The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 
+    /// The bst instruction (opcode 2) calculates the value of its combo operand modulo 8
     /// (thereby keeping only its lowest 3 bits), then writes that value to the B register.
     fn bst(&mut self, operand: i64) {
         let x = self.combo_operand(operand).unwrap();
@@ -127,10 +126,10 @@ impl Computer {
         self.ip += 2;
     }
 
-    /// The jnz instruction (opcode 3) does nothing if the A register is 0. 
-    /// However, if the A register is not zero, it jumps by setting the 
-    /// instruction pointer to the value of its literal operand; 
-    /// if this instruction jumps, the instruction pointer is not 
+    /// The jnz instruction (opcode 3) does nothing if the A register is 0.
+    /// However, if the A register is not zero, it jumps by setting the
+    /// instruction pointer to the value of its literal operand;
+    /// if this instruction jumps, the instruction pointer is not
     /// increased by 2 after this instruction.
     fn jnz(&mut self, operand: i64) {
         if self.reg_a == 0 {
@@ -140,19 +139,19 @@ impl Computer {
         }
     }
 
-    /// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, 
-    /// then stores the result in register B. (For legacy reasons, this instruction 
+    /// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C,
+    /// then stores the result in register B. (For legacy reasons, this instruction
     /// reads an operand but ignores it.)
     fn bxc(&mut self, _operand: i64) {
         self.reg_b = self.reg_b ^ self.reg_c;
         self.ip += 2;
     }
 
-    /// The out instruction (opcode 5) calculates the value of its combo operand modulo 8, 
+    /// The out instruction (opcode 5) calculates the value of its combo operand modulo 8,
     /// then outputs that value. (If a program outputs multiple values, they are separated by commas.)
     fn out(&mut self, operand: i64) {
         // TODO: check for any negative numbers
-        let x = self.combo_operand(operand).unwrap() & 0x7; 
+        let x = self.combo_operand(operand).unwrap() & 0x7;
         self.output.push(x as u8);
         if self.output.len() > 100 {
             panic!("output too long");
@@ -173,7 +172,7 @@ impl Computer {
             5 => self.out(operand),
             6 => self.bdv(operand),
             7 => self.cdv(operand),
-            _ => panic!("unexpected instruction {inst}")
+            _ => panic!("unexpected instruction {inst}"),
         }
     }
 
@@ -187,10 +186,6 @@ impl Computer {
         self.output.iter().join(",")
     }
 }
-
-
-
-
 
 fn part1(mut computer: Computer) -> Result<String> {
     computer.run_program();
@@ -230,11 +225,68 @@ fn part2(computer: &Computer) -> Result<i64> {
 
         // check end state
         if candidate.output == computer.program {
-            return Ok(init_a)
+            return Ok(init_a);
         }
     }
 
     bail!("No solution found")
+}
+
+fn part_2_hardcoded(computer: Computer) -> Result<i64> {
+    fn single_loop(a: i64) -> (i64, i64) {
+        let b = a & 0x7;
+        let b = b ^ 2;
+        let c = a >> b;
+        let b = b ^ c;
+        let b = b ^ 3;
+        let res = b & 0x7;
+        let a = a >> 3;
+        (res, a)
+    }
+
+    fn find_a_for(start:i64, target_out: i64, target_a: i64) -> i64 {
+        (start..)
+            .find(|i| {
+                if i % 1_000_000_000 == 0 {
+                    print!(".");
+                }
+                let (res, a) = single_loop(*i);
+                res == target_out && a == target_a
+            })
+            .unwrap()
+    }
+
+    //let mut init_a = 0;
+    println!("{:?}", computer.program);
+    // verify first run
+    let mut a = computer.reg_a;
+    while a > 0 {
+        let (res, a2) = single_loop(a);
+        println!("{a} -> {res}");
+        a = a2;
+    }
+
+    println!("-- search --");
+
+    let mut target_a = 0;
+    for n in computer.program.iter().rev().copied() {
+        let start = 0 << 3;
+        let target_out = n as i64;
+        let a = find_a_for(start, target_out, target_a);
+        target_a = a;
+        println!("{a} -> {target_out}");
+        //init_a = (init_a << 3) + a;
+
+    }
+    let test_computer = Computer {
+        reg_a: target_a,
+        reg_b: 0, 
+        reg_c: 0,
+        .. computer.clone()
+    };
+    println!("{:?}", part1(test_computer)?);
+
+    Ok(target_a)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -246,6 +298,11 @@ fn main() -> anyhow::Result<()> {
     println!("Part 1 result is {res_part1} (took {:?})", t1.elapsed());
 
     let t2 = Instant::now();
+    let res_part2_hardcoded = part_2_hardcoded(problem.clone())?;
+    println!(
+        "Part 2 result is {res_part2_hardcoded} (took {:?})",
+        t2.elapsed()
+    );
     let res_part2 = part2(&problem)?;
     println!("Part 2 result is {res_part2} (took {:?})", t2.elapsed());
 
@@ -273,12 +330,10 @@ mod tests {
         Program: 0,3,5,4,3,0
     "};
 
-    
-
     // If register C contains 9, the program 2,6 would set register B to 1.
     #[test]
     fn case1() {
-        let mut computer = Computer::new(0,0,9, vec![2,6]);
+        let mut computer = Computer::new(0, 0, 9, vec![2, 6]);
         computer.run_program();
         assert_eq!(computer.reg_b, 1);
     }
@@ -286,16 +341,16 @@ mod tests {
     // If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
     #[test]
     fn case2() {
-        let mut computer = Computer::new(10,0,0, vec![5,0,5,1,5,4]);
+        let mut computer = Computer::new(10, 0, 0, vec![5, 0, 5, 1, 5, 4]);
         computer.run_program();
         assert_eq!(computer.format_output(), "0,1,2");
     }
 
-    // If register A contains 2024, the program 0,1,5,4,3,0 would 
+    // If register A contains 2024, the program 0,1,5,4,3,0 would
     // output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
     #[test]
     fn case3() {
-        let mut computer = Computer::new(2024,0,0, vec![0,1,5,4,3,0]);
+        let mut computer = Computer::new(2024, 0, 0, vec![0, 1, 5, 4, 3, 0]);
         computer.run_program();
         assert_eq!(computer.format_output(), "4,2,5,6,7,7,7,7,3,1,0");
         assert_eq!(computer.reg_a, 0);
@@ -304,7 +359,7 @@ mod tests {
     // If register B contains 29, the program 1,7 would set register B to 26.
     #[test]
     fn case4() {
-        let mut computer = Computer::new(0,29,0, vec![1,7]);
+        let mut computer = Computer::new(0, 29, 0, vec![1, 7]);
         computer.run_program();
         assert_eq!(computer.reg_b, 26);
     }
@@ -312,7 +367,7 @@ mod tests {
     // If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
     #[test]
     fn case5() {
-        let mut computer = Computer::new(0,2024,43690, vec![4,0]);
+        let mut computer = Computer::new(0, 2024, 43690, vec![4, 0]);
         computer.run_program();
         assert_eq!(computer.reg_b, 44354);
     }
